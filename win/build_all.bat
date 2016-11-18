d2530f15b0c8f034147edc1055313f20034f109b
Henric Jungheim <software@henric.org>
2016-11-17 17:48:32 -0700

    Add VS2017 support.

--- win/build_all.bat
+++ win/build_all.bat
@@ -4,8 +4,14 @@ call "%~dp0build_wx2013.bat"
 call "%~dp0build_wx2015.bat"
 @if %errorlevel% neq 0 exit /b %errorlevel%
 
+call "%~dp0build_wx2017.bat"
+@if %errorlevel% neq 0 exit /b %errorlevel%
+
 call "%~dp0build_2013.bat"
 @if %errorlevel% neq 0 exit /b %errorlevel%
 
 call "%~dp0build_2015.bat"
+@if %errorlevel% neq 0 exit /b %errorlevel%
+
+call "%~dp0build_2017.bat"
 @if %errorlevel% neq 0 exit /b %errorlevel%
