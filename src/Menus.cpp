556123a2816a79165ada8d7740dd1884b663aa0d
Pokechu22 <Pokechu022@gmail.com>
2017-04-11 09:16:33 -0700

    Fix Fit to Height not resizing note tracks

--- src/Menus.cpp
+++ src/Menus.cpp
@@ -5793,7 +5793,7 @@ void AudacityProject::DoZoomFitV()
    TrackListIterator iter(GetTracks());
    Track *t = iter.First();
    while (t) {
-      if ((t->GetKind() == Track::Wave) &&
+      if ((nullptr != dynamic_cast<AudioTrack*>(t)) &&
           !t->GetMinimized())
          count++;
       else
@@ -5813,7 +5813,7 @@ void AudacityProject::DoZoomFitV()
    TrackListIterator iter2(GetTracks());
    t = iter2.First();
    while (t) {
-      if ((t->GetKind() == Track::Wave) &&
+      if ((nullptr != dynamic_cast<AudioTrack*>(t)) &&
           !t->GetMinimized())
          t->SetHeight(height);
       t = iter2.Next();
