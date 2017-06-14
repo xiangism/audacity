8750fd87bef1b8ca39c484a43dc5f874069f3247
Henric Jungheim <software@henric.org>
2017-06-14 08:42:19 -0700

    Add an MSBuild top.proj. It builds both the wxWidgets submodule and Audacity, doing the required property magic to get things building properly.

--- win/build_wx2015.bat
+++ win/build_wx2015.bat
@@ -1,39 +1,76 @@
 @setlocal
 
+@if exist "%~dp0build\" goto skipmd
+
 md "%~dp0build\"
-
-call "%VS140COMNTOOLS%vsvars32.bat"
 @if %errorlevel% neq 0 goto errorexit
 
-cd "%WXWIN_VS2015%\build\msw\"
+:skipmd
 
-msbuild /m /p:Configuration="DLL Release",Platform=x64 /t:Build /consoleLoggerParameters:Summary /verbosity:minimal /fileLogger /fileLoggerParameters:Summary;Append;Verbosity=normal;LogFile=%~dp0build\wx_x64_dll_2015.log wx_vc14.sln
+@if NOT "%WXWIN_VS2015%"=="" (
+   set WXWIN=%WXWIN_VS2015%
+)
+
+@if "%WXWIN%"=="" (
+   set WXWIN=%~dp0..\wxWidgets
+)
+
+@if exist "%WXWIN%\include\wx\msw\setup.h" goto skipcopy
+
+copy "%WXWIN%\include\wx\msw\setup0.h" "%WXWIN%\include\wx\msw\setup.h"
 @if %errorlevel% neq 0 goto errorexit
 
-msbuild /m /p:Configuration="DLL Release",Platform=Win32 /t:Build /consoleLoggerParameters:Summary /verbosity:minimal /fileLogger /fileLoggerParameters:Summary;Append;Verbosity=normal;LogFile=%~dp0build\wx_win32_dll_2015.log wx_vc14.sln
+:skipcopy
+
+@setlocal
+
+call "%VS140COMNTOOLS%VsDevCmd.bat"
 @if %errorlevel% neq 0 goto errorexit
 
-msbuild /m /p:Configuration="Release",Platform=x64 /t:Build /consoleLoggerParameters:Summary /verbosity:minimal /fileLogger /fileLoggerParameters:Summary;Append;Verbosity=normal;LogFile=%~dp0build\wx_x64_2015.log wx_vc14.sln
+call :dobuild Win32 v140_xp
 @if %errorlevel% neq 0 goto errorexit
 
-msbuild /m /p:Configuration="Release",Platform=Win32 /t:Build /consoleLoggerParameters:Summary /verbosity:minimal /fileLogger /fileLoggerParameters:Summary;Append;Verbosity=normal;LogFile=%~dp0build\wx_win32_2015.log wx_vc14.sln
+@endlocal
+@setlocal
+
+call "%VS140COMNTOOLS%VsDevCmd.bat" -arch=x64 -host_arch=x64
 @if %errorlevel% neq 0 goto errorexit
 
-msbuild /m /p:Configuration="DLL Debug",Platform=x64 /t:Build /consoleLoggerParameters:Summary /verbosity:minimal /fileLogger /fileLoggerParameters:Summary;Append;Verbosity=normal;LogFile=%~dp0build\wxd_x64_dll_2015.log wx_vc14.sln
-@if %errorlevel% neq 0 goto errorexit
-
-msbuild /m /p:Configuration="DLL Debug",Platform=Win32 /t:Build /consoleLoggerParameters:Summary /verbosity:minimal /fileLogger /fileLoggerParameters:Summary;Append;Verbosity=normal;LogFile=%~dp0build\wxd_win32_dll_2015.log wx_vc14.sln
-@if %errorlevel% neq 0 goto errorexit
-
-msbuild /m /p:Configuration="Debug",Platform=x64 /t:Build /consoleLoggerParameters:Summary /verbosity:minimal /fileLogger /fileLoggerParameters:Summary;Append;Verbosity=normal;LogFile=%~dp0build\wxd_x64_2015.log wx_vc14.sln
-@if %errorlevel% neq 0 goto errorexit
-
-msbuild /m /p:Configuration="Debug",Platform=Win32 /t:Build /consoleLoggerParameters:Summary /verbosity:minimal /fileLogger /fileLoggerParameters:Summary;Append;Verbosity=normal;LogFile=%~dp0build\wxd_win32_2015.log wx_vc14.sln
+call :dobuild x64 v140
 @if %errorlevel% neq 0 goto errorexit
 
 @endlocal
 @exit /b 0
 
+:dobuild
+
+cd "%WXWIN%\build\msw\"
+@if %errorlevel% neq 0 goto errorexit
+
+call :run_msbuild Debug %1 %2
+@if %errorlevel% neq 0 goto errorexit
+
+call :run_msbuild "DLL Debug" %1 %2
+@if %errorlevel% neq 0 goto errorexit
+
+call :run_msbuild "Release" %1 %2 ";WholeProgramOptimization=true"
+@if %errorlevel% neq 0 goto errorexit
+
+call :run_msbuild "DLL Release" %1 %2 ";WholeProgramOptimization=true"
+@if %errorlevel% neq 0 goto errorexit
+
+@exit /b 0
+
+:run_msbuild
+
+@echo +++++++++++++++++++++++++ %~1 %~2 %~3
+msbuild /m "/p:Configuration=%~1;Platform=%~2;PlatformToolset=%~3;wxCompilerPrefix=%~3%~4" /t:Build /consoleLoggerParameters:Summary /verbosity:minimal /fileLogger "/fileLoggerParameters:Summary;Append;Verbosity=normal;LogFile=%~dp0build\wx_%~1_%~2_%~3.log" wx_vc12.sln
+@if %errorlevel% neq 0 goto errorexit
+@echo ------------------------- %~1 %~2 %~3
+
+@exit /b 0
+
 :errorexit
 @endlocal
+@echo ***** BUILD FAILED ***** %0 %*
 @exit /b 1
--- win/build_wx2017.bat
+++ win/build_wx2017.bat
@@ -1,5 +1,24 @@
 @setlocal
 
+@if exist "%VS150COMNTOOLS%VsDevCmd.bat" goto skipwhere
+
+@set VSWHERE="%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe"
+
+@if not exist %VSWHERE% set VSWHERE="%ProgramFiles%\Microsoft Visual Studio\Installer\vswhere.exe"
+
+@if not exist %VSWHERE% goto errorexit
+
+@set pre=Microsoft.VisualStudio.Product.
+@set ids=%pre%Community %pre%Professional %pre%Enterprise %pre%BuildTools
+
+@for /f "usebackq tokens=*" %%i in (`%VSWHERE% -latest -products %ids% -requires Microsoft.Component.MSBuild -requires Microsoft.VisualStudio.Workload.NativeDesktop -property installationPath`) do @(
+  @set VS150COMNTOOLS=%%i\Common7\Tools\
+)
+
+@if not exist "%VS150COMNTOOLS%VsDevCmd.bat" goto errorexit
+
+:skipwhere
+
 @if exist "%~dp0build\" goto skipmd
 
 md "%~dp0build\"
@@ -64,7 +83,7 @@ call :run_msbuild "DLL Release" %1 %2 ";WholeProgramOptimization=true"
 :run_msbuild
 
 @echo +++++++++++++++++++++++++ %~1 %~2 %~3
-msbuild /m "/p:Configuration=%~1;Platform=%~2;PlatformToolset=%~3;wxCompilerPrefix=%~3%~4" /t:Build /consoleLoggerParameters:Summary /verbosity:minimal /fileLogger "/fileLoggerParameters:Summary;Append;Verbosity=normal;LogFile=%~dp0build\wx_%~1_%~2_%~3.log" wx_vc12.sln
+msbuild /m "/p:Configuration=%~1;Platform=%~2;PlatformToolset=%~3;wxCompilerPrefix=%~3%~4" /t:Build /consoleLoggerParameters:Summary;PerformanceSummary /verbosity:minimal /fileLogger "/fileLoggerParameters:Summary;PerformanceSummary;Append;Verbosity=normal;LogFile=%~dp0build\wx_%~1_%~2_%~3.log" wx_vc12.sln
 @if %errorlevel% neq 0 goto errorexit
 @echo ------------------------- %~1 %~2 %~3
 
