456e2dc0d5ed3c98b6a898e1a40838e124fce265
Henric Jungheim <software@henric.org>
2017-10-20 13:23:11 -0700

    Add ARM64 configuration.

--- win/clean_top.bat
+++ win/clean_top.bat
@@ -40,6 +40,15 @@ call :dobuild x64
 @if %errorlevel% neq 0 goto errorexit
 
 @endlocal
+@setlocal
+
+call "%VS150COMNTOOLS%VsDevCmd.bat" -arch=arm64 -host_arch=x64
+@if %errorlevel% neq 0 goto weberrorexit
+
+call :dobuild ARM64
+@if %errorlevel% neq 0 goto errorexit
+
+@endlocal
 @exit /b 0
 
 :dobuild
