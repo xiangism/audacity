aee59e63f7cb44e89b48b760c016606aeae9b9a0
Henric Jungheim <software@henric.org>
2018-01-01 16:39:54 -0700

    Fix libnyquist compiler warnings.

--- lib-src/libnyquist/nyquist/xlisp/xlisp.h
+++ lib-src/libnyquist/nyquist/xlisp/xlisp.h
@@ -43,11 +43,15 @@ extern "C" {
 /* for the Win32 environment */
 #ifdef WIN32
 #define NNODES		2000
-#define AFMT		"%lx"
-#define OFFTYPE		long
+#define AFMT		"%p"
+#define OFFTYPE		intptr_t
 #define SAVERESTORE
 #define XL_LITTLE_ENDIAN 
 #define _longjmp longjmp
+#define FIXTYPE intptr_t
+#if defined(_M_X64) || defined(_M_IX86)
+#define XL_LITTLE_ENDIAN
+#endif
 #endif
 
 /* for the Turbo C compiler - MS-DOS, large model */
