67ac0f2698c743bd057cfe4e7490b2135492735b
Henric Jungheim <software@henric.org>
2018-01-11 05:58:45 -0700

    Clobber rand() infestations.

--- src/DirManager.h
+++ src/DirManager.h
@@ -13,6 +13,8 @@
 
 #include "audacity/Types.h"
 #include "xml/XMLTagHandler.h"
+#include "wxFileNameWrapper.h"
+#include "RngSupport.h"
 
 #include <functional>
 #include <unordered_map>
@@ -264,6 +266,12 @@ class PROFILE_DLL_API DirManager final : public XMLTagHandler {
    wxString mytemp;
    static int numDirManagers;
    static bool dontDeleteTempFiles;
+
+   Nyq::NyqEngine<> generator;
+   std::uniform_int_distribution<> distribution1M{ 0, 1 << 20 };
+   std::uniform_int_distribution<> distribution255{ 0, 255 };
+   std::uniform_int_distribution<> distribution4095{ 0, 4095 };
+   std::uniform_int_distribution<> distribution_rand_max{ 0, RAND_MAX };
 };
 
 #endif
