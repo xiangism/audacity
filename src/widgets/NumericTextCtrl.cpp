fb0d28e572ebaa533e367ab17c1efc48a1cee8df
Henric Jungheim <software@henric.org>
2018-01-16 01:43:35 -0700

    Draw NumericTextCtrl LTR. This isn't right, but at least the text is readable.

--- src/widgets/NumericTextCtrl.cpp
+++ src/widgets/NumericTextCtrl.cpp
@@ -1447,6 +1447,8 @@ bool NumericTextCtrl::Layout()
 
    wxMemoryDC memDC;
 
+   memDC.SetLayoutDirection(wxLayout_LeftToRight);
+
    // Placeholder bitmap so the memDC has something to reference
    mBackgroundBitmap = std::make_unique<wxBitmap>(1, 1, 24);
    memDC.SelectObject(*mBackgroundBitmap);
@@ -1575,6 +1577,9 @@ void NumericTextCtrl::OnErase(wxEraseEvent & WXUNUSED(event))
 void NumericTextCtrl::OnPaint(wxPaintEvent & WXUNUSED(event))
 {
    wxPaintDC dc(this);
+
+   dc.SetLayoutDirection(wxLayout_LeftToRight);
+
    bool focused = (FindFocus() == this);
 
    dc.DrawBitmap(*mBackgroundBitmap, 0, 0);
