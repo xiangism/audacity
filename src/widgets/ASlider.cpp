378c95fe1784044e9c16fa2373385bbc1a620576
Henric Jungheim <software@henric.org>
2018-01-16 01:41:31 -0700

    ASlider should not behave as badly with RTL locales.

--- src/widgets/ASlider.cpp
+++ src/widgets/ASlider.cpp
@@ -663,6 +663,12 @@ void LWSlider::OnPaint(wxDC &dc, bool highlight)
       dc.Clear();
    }
 
+   const auto direction = dc.GetLayoutDirection();
+   const auto is_rtl = direction == wxLayout_RightToLeft;
+
+   if (is_rtl)
+      dc.SetLayoutDirection(wxLayout_LeftToRight);
+
    dc.DrawBitmap(*mBitmap, mLeft, mTop, true);
    const auto &thumbBitmap =
       highlight ? *mThumbBitmapHilited : *mThumbBitmap;
@@ -675,6 +681,9 @@ void LWSlider::OnPaint(wxDC &dc, bool highlight)
       // TODO: Don't use pixel-count hack in positioning.  
       dc.DrawBitmap(thumbBitmap, mLeft+thumbOrtho-5, mTop+thumbPos, true);
    }
+
+   if (is_rtl)
+      dc.SetLayoutDirection(direction);
 
    if (mTipPanel)
    {
@@ -1123,7 +1132,10 @@ void LWSlider::OnMouseEvent(wxMouseEvent & event)
    wxRect r;
    if (mOrientation == wxHORIZONTAL)
    {
-      r.x = mLeft + ValueToPosition(mCurrentValue);
+      if (mParent->GetLayoutDirection() == wxLayout_RightToLeft)
+         r.x = mRightX - ValueToPosition(mCurrentValue);
+      else
+         r.x = mLeft + ValueToPosition(mCurrentValue);
       r.y = mTop + (mCenterY - (mThumbHeight / 2));
    }
    else
@@ -1346,7 +1358,11 @@ float LWSlider::ClickPositionToValue(int fromPos, bool shiftDown)
    int pos;
    if (mOrientation == wxHORIZONTAL)
    {
-      pos = (fromPos - mLeft - (mThumbWidth / 2));
+      if (mParent->GetLayoutDirection() == wxLayout_RightToLeft)
+         pos = (mRightX - fromPos);
+      else
+         pos = (fromPos - mLeft - (mThumbWidth / 2));
+
       nSpan = mWidthX;
    }
    else
@@ -1387,6 +1403,9 @@ float LWSlider::ClickPositionToValue(int fromPos, bool shiftDown)
 float LWSlider::DragPositionToValue(int fromPos, bool shiftDown)
 {
    int delta = (fromPos - mClickPos);
+
+   if (mOrientation == wxHORIZONTAL && mParent->GetLayoutDirection() == wxLayout_RightToLeft)
+      delta = (mClickPos - fromPos);
 
    float speed = mSpeed;
    // Precision enhancement for Shift drags
