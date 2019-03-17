e33c1394d86451a519c3c80b8bf6bbc8469a19a5
Henric Jungheim <software@henric.org>
2019-03-17 05:44:48 -0700

    Fix ProgressDialog.Update() overload resolution on x64 builds.

--- src/DirManager.cpp
+++ src/DirManager.cpp
@@ -334,7 +334,7 @@ void DirManager::RecursivelyRemove(const FilePaths& filePathArray, int count, in
          }
       }
       if (progress)
-         progress->Update(ii + bias, count);
+         progress->Update(ii + bias, static_cast<wxULongLong_t>(count));
    }
 }
 
@@ -690,7 +690,7 @@ DirManager::ProjectSetter::Impl::Impl(
       ProgressDialog progress(_("Progress"),
          _("Saving project data files"));
 
-      int total = dirManager.mBlockFileHash.size();
+      const wxULongLong_t total = dirManager.mBlockFileHash.size();
 
       bool link = moving;
       for (const auto &pair : dirManager.mBlockFileHash) {
