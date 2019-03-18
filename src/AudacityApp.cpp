b4bcbf6b10d0dde7854f841f00ab4e0f470134bf
Henric Jungheim <software@henric.org>
2019-03-17 17:14:48 -0700

    Use build files from vcxproj files.

--- src/AudacityApp.cpp
+++ src/AudacityApp.cpp
@@ -208,6 +208,7 @@ It handles initialization and termination by subclassing wxApp.
 #     pragma comment(lib, "libvamp")
 #  endif
 
+#if AUDACITY_NEED_WX_LIBS
 #  if defined(_DEBUG)
 #     define D "d"
 #  else
@@ -236,7 +237,7 @@ It handles initialization and termination by subclassing wxApp.
 
 #  undef V
 #  undef D
-
+#endif // AUDACITY_NO_DLLEXPORT
 #endif //(__WXMSW__)
 
 // DA: Logo for Splash Screen
